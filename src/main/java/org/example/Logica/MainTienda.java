9907cda39eb2d3fda581205750b84d3afdd858c9
Kmiktt <kmilagato05@gmail.com>
2025-07-06 18:51:03 -0400

    ejemplo de refugio agregado

--- src/main/java/org/example/Logica/MainTienda.java
+++ src/main/java/org/example/Logica/MainTienda.java
@@ -22,35 +22,25 @@ public class MainTienda {
             System.out.println(tienda.getPrecios().get(i));
         }
 
-        //compras el primer objeto de la tienda de objetos
+        //compras el segundo objeto de la tienda de objetos
         u.agregarObjeto(tienda.comprarProducto(1));
 
         System.out.println(u.getInventario());
         System.out.println(u.getDinero());
 
         //tienda de mascotas (para demostrar la otra manera de comprar)
-        Tienda<Mascota> tiendamasc=new Tienda<Mascota>();
-        //agregar productos
-        tiendamasc.agregarProducto(new Gato("Pedro","nose",1),10);
-        tiendamasc.agregarProducto(new Perro("Juan","nose",1),10);
-        tiendamasc.agregarProducto(new Pez("Diego","nose",1),10);
-        tiendamasc.agregarProducto(new Gato("wiwi","nose",1),10);
+        Refugio tiendamasc=new Refugio();
+        Habitat tierra=new Habitat("terrestre");
         //restock
+        tiendamasc.actualizarAnimales();
         tiendamasc.actualizarStock(2);
-
         for (int i = 0; i < tiendamasc.getStock().size(); i++) {
-            System.out.print(tiendamasc.getStock().get(i) + " ");
-            System.out.println(tiendamasc.getPrecios().get(i));
+            System.out.println(tiendamasc.getStock().get(i) + " ");
         }
         //2da forma de usar comprar producto con 2 argumentos
-        u.tomarMascota(tiendamasc.comprarProducto(1,u.getMascota()));
+        tiendamasc.adoptarAnimal(0,"sigma skibidi II");
 
         System.out.println("\n"+u.getMascota());
-        System.out.println(u.getDinero());
 
-        //no gasta plata ni reemplaza la mascota a intentar comprar denuevo
-        u.tomarMascota(tiendamasc.comprarProducto(2,u.getMascota()));
-        System.out.println("\n"+u.getMascota());
-        System.out.println(u.getDinero());
     }
 }
