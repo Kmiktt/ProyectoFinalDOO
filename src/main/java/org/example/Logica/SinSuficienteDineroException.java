e1e006b27869704a459303a00f67f273dfc5b48f
Kmiktt <kmilagato05@gmail.com>
2025-07-13 07:17:18 -0400

    JavaDoc agregado

--- src/main/java/org/example/Logica/SinSuficienteDineroException.java
+++ src/main/java/org/example/Logica/SinSuficienteDineroException.java
@@ -1,5 +1,7 @@
 package org.example.Logica;
 
+/** Es lanzado cuando el usuario no tiene suficiente dinero para hacer una compra
+ */
 public class SinSuficienteDineroException extends RuntimeException {
     public SinSuficienteDineroException() {
         super();
