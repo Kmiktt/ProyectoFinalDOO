18b6dac39b6cb71b91843a47d119e391dde41913
Kmiktt <kmilagato05@gmail.com>
2025-07-13 20:42:48 -0400

    Excepcion agregada

---
+++ src/main/java/org/example/Logica/ConsumibleNuloException.java
@@ -0,0 +1,9 @@
+package org.example.Logica;
+
+/** Excepcion cuando se intenta interactuar con un consumible nulo
+ */
+public class ConsumibleNuloException extends RuntimeException {
+    public ConsumibleNuloException() {
+        super();
+    }
+}
